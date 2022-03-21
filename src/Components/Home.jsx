315b33e18e80a9493af82879947737a7f1558202
Mark <markokoth96@gmail.com>
2022-03-21 15:14:44 +0300

    initial deploy

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,10 +4,11 @@ import "./Home.css";
 function Home() {
   return (
     <div className="Home">
-      <div className=" flex flex-col  text-white min-w-max justify-center items-center min-h-full gap-3">
-        <p className="font-bold text-5xl">Mark Okoth</p>
-        <p className="text-lg">
-          Front-End Developer | Business Central | IT Technical Consultant
+      <div className="con flex flex-col  text-white min-w-max justify-center items-center min-h-full gap-3 ">
+        <p className="font-bold text-5xl header ">Mark Okoth</p>
+        <p className="text-lg content">
+          <span> Front-End Developer </span>| <span> Business Central</span> |
+          <span> IT Technical Consultant</span>
         </p>
         <a
           className="bg-blue-300 p-3 uppercase text-xs"
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -20,7 +20,7 @@ function Nav() {
               </span>
             </a>
             <div className="flex md:order-2">
-              <a href={Resume}>
+              <a href={Resume} target={'_blank'}>
                 <button
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
