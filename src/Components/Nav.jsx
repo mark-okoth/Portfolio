d1247908cda9050c2828d3fdaef89a137df3f06a
Mark <markokoth96@gmail.com>
2022-03-21 16:07:46 +0300

    removing all errors and warnings

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,5 +1,4 @@
-import Logo from "../logo.svg";
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import Resume from "./MarkResume.pdf";
 function Nav() {
@@ -9,7 +8,7 @@ function Nav() {
       <header className="App-header">
         <nav className="bg-blue-300 border-gray-200 px-2 sm:px-4 py-2.5 rounded">
           <div className="container flex flex-wrap justify-between items-center mx-auto">
-            <a href="#" className="flex items-center">
+            <a href={'/'} className="flex items-center">
               {/* <img
                 src={Logo}
                 className="mr-3 h-6 sm:h-10 bg-black"
@@ -20,7 +19,7 @@ function Nav() {
               </span>
             </a>
             <div className="flex md:order-2">
-              <a href={Resume} target={'_blank'}>
+              <a href={Resume}  rel="noreferrer" target={'_blank'}>
                 <button
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -78,7 +77,7 @@ function Nav() {
               <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                 <li>
                   <a
-                    href="#"
+                    href={'/'}
                     className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
                     aria-current="page"
                   >
@@ -87,7 +86,7 @@ function Nav() {
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href={'/'}
                     className="block py-2 pr-4 pl-3 text-white  hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     ABOUT
@@ -95,7 +94,7 @@ function Nav() {
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href={'/'}
                     className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     SKILLS
@@ -103,7 +102,7 @@ function Nav() {
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href={'/'}
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     CONTACT
@@ -128,28 +127,28 @@ function Nav() {
                   className="px-2 pt-2 pb-3 space-y-1 sm:px-3 uppercase"
                 >
                   <a
-                    href="#"
+                    href={'/'}
                     className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
                   >
                     HOME
                   </a>
 
                   <a
-                    href="#"
+                    href={'/'}
                     className="text-white hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                   >
                     ABOUT
                   </a>
 
                   <a
-                    href="#"
+                    href={'/'}
                     className="text-white hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                   >
                     Skills
                   </a>
 
                   <a
-                    href="#"
+                    href={'/'}
                     className="text-white hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                   >
                     CONTACT
