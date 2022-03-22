374c45fb262b4bc0783c2623dce5d27aa6864b4b
Mark <markokoth96@gmail.com>
2022-03-22 09:42:50 +0300

    skills section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Nav from "./Components/Nav";
 import Home from "./Components/Home";
 import Footer from './Components/Footer'
+import Skills from "./Components/Skill";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
         <Home/>
+        <Skills/>
         <Footer/>
       </header>
     </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,12 +7,13 @@ function Home() {
       <div className="con flex flex-col  text-white min-w-max justify-center items-center min-h-full gap-3 ">
         <p className="font-bold text-5xl header">Mark Okoth</p>
         <p className="text-lg content">
-          <span> Front-End Developer </span>| <span> Business Central Developer</span> |
+          <span> Front-End Developer </span>|{" "}
+          <span> Business Central Developer</span> |
           <span> IT Technical Consultant</span>
         </p>
         <a
           className="bg-blue-300 p-3 uppercase text-xs"
-          href={'https://www.linkedin.com/in/mark-okoth-1b514115b/'}
+          href={"https://www.linkedin.com/in/mark-okoth-1b514115b/"}
           target={`_blank`}
         >
           Connect via LINKEDIN
