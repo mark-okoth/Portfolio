e9bc49d558590692f467f6c2b96ac67fffc9c1cb
Mark <markokoth96@gmail.com>
2022-03-24 12:08:45 +0300

    adding projects on github

--- src/data.js
+++ src/data.js
@@ -1,35 +1,34 @@
 export const projects = [
   {
-    title: "Linkedin Clone",
+    title: "Gmail Clone and Material UI",
     subtitle: "React",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      "A clone of Gmail with Material UI. This is a full-stack application",
     image: "https://media.gcflearnfree.org/content/55e073847dd48174331f5193_01_17_2014/intro_inbox_gmail2.jpg",
-    link: "https://github.com/mark-okoth",
+    link: "https://github.com/mark-okoth/Gmail-Clone/tree/main/src",
   },
   {
-    title: "E-commerce Website",
+    title: "E-commerce Website React",
     subtitle: "React",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: 'https://media.gcflearnfree.org/content/55e073847dd48174331f5193_01_17_2014/intro_inbox_gmail2.jpg',
-    link: "https://github.com/mark-okoth",
+      "A full-stack e-commerce website with React using the fake e-commerce API",
+    image: 'https://assets.wpdeveloper.com/2019/04/How-to-Design-Homepage-of-Your-eCommerce-Site-To-Boost-Sales.png',
+    link: "https://github.com/mark-okoth/ReactEcommerce/tree/main",
   },
   {
-    title: "Gmail clone",
-    subtitle: "React and material UI",
+    title: "Django E-commerce Website",
+    subtitle: "Django",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "https://media.gcflearnfree.org/content/55e073847dd48174331f5193_01_17_2014/intro_inbox_gmail2.jpg",
+      "django e-commerce website with Django and Bootstrap",
+    image: "https://cdn-screenshots.comidoc.net/1952540_4.png",
     link: "https://github.com/mark-okoth",
   },
   {
     title: "E-Commerce Website",
     subtitle: "Django",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "https://media.gcflearnfree.org/content/55e073847dd48174331f5193_01_17_2014/intro_inbox_gmail2.jpg",
-    link: "https://github.com/mark-okoth",
+    description:"Django-rest-framework weather api using apis consuming with react",
+    image: "https://www.xda-developers.com/files/2020/06/https___i.imgur_.com_vUw6udW.jpg",
+    link: "https://github.com/mark-okoth/weather-rest-api",
   },
 ];
 
