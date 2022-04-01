6950c349319b6cc09e47ff55ead8891aa4cdea14
Mark <markokoth96@gmail.com>
2022-04-01 14:14:39 +0300

    added a new project to the list

--- src/data.js
+++ src/data.js
@@ -4,7 +4,8 @@ export const projects = [
     subtitle: "React",
     description:
       "A clone of Gmail with Material UI. This is a full-stack application",
-    image: "https://media.gcflearnfree.org/content/55e073847dd48174331f5193_01_17_2014/intro_inbox_gmail2.jpg",
+    image:
+      "https://media.gcflearnfree.org/content/55e073847dd48174331f5193_01_17_2014/intro_inbox_gmail2.jpg",
     link: "https://github.com/mark-okoth/Gmail-Clone/tree/main/src",
   },
   {
@@ -12,22 +13,25 @@ export const projects = [
     subtitle: "React",
     description:
       "A full-stack e-commerce website with React using the fake e-commerce API",
-    image: 'https://assets.wpdeveloper.com/2019/04/How-to-Design-Homepage-of-Your-eCommerce-Site-To-Boost-Sales.png',
+    image:
+      "https://assets.wpdeveloper.com/2019/04/How-to-Design-Homepage-of-Your-eCommerce-Site-To-Boost-Sales.png",
     link: "https://github.com/mark-okoth/ReactEcommerce/tree/main",
   },
   {
-    title: "Django E-commerce Website",
-    subtitle: "Django",
+    title: "Work Out Application",
+    subtitle: "React native and laravel Api app",
     description:
-      "django e-commerce website with Django and Bootstrap",
-    image: "https://cdn-screenshots.comidoc.net/1952540_4.png",
-    link: "https://github.com/mark-okoth",
+      "React Native and laravel api app the enable people in planning out work out activity inspired by dribble design",
+    image: "https://cdn.dribbble.com/users/5031392/screenshots/14045799/media/fa30af70c68b439e17aec0a0d5b6a096.png?compress=1&resize=1200x900&vertical=top",
+    link: "https://github.com/mark-okoth/React-Native--WorkOutApp",
   },
   {
-    title: "E-Commerce Website",
+    title: "Wether app API and django",
     subtitle: "Django",
-    description:"Django-rest-framework weather api using apis consuming with react",
-    image: "https://www.xda-developers.com/files/2020/06/https___i.imgur_.com_vUw6udW.jpg",
+    description:
+      "Django-rest-framework weather api. The api is consumed on the frontend using axios for the react application",
+    image:
+      "https://www.xda-developers.com/files/2020/06/https___i.imgur_.com_vUw6udW.jpg",
     link: "https://github.com/mark-okoth/weather-rest-api",
   },
 ];
@@ -37,18 +41,20 @@ export const skills = [
   "CSS",
   "Javascript",
   "React",
-  'React Hooks',
+  "React Native",
+  "React Hooks",
   "CI/CD",
-  'TDD',
+  "TDD",
   "Jest",
   "React Testing Library",
   "Python",
-  'Django',
-  'Django-rest-framework',
-  'TailwindCSS',
-  'Git',
-  'Material UI',
-  'Basics Android Development with Kotlin',
-  'C/AL & AL',
-  'Business Central ERP'
+  "Django",
+  "Django-rest-framework",
+  "TailwindCSS",
+  "Git",
+  "Material UI",
+  "Basics Android Development with Kotlin",
+  "C/AL & AL",
+  "Business Central ERP",
+  "PHP Laravel"
 ];
